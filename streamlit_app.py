f6c4257c9f2d1ee17eb9c2a0f1008a32ea45225f
CHANDHAN KUMAR <chandhankumar356@gmail.com>
2026-08-21 15:40:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,12 @@ from sklearn.linear_model import LogisticRegression
 
 
 # --------------------------------------------------
-# PAGE TITLE
+# TITLE
 # --------------------------------------------------
 
-st.title('MACHINE LEARNING APP')
+st.title("MACHINE LEARNING APP")
 
-st.info('THIS IS A MACHINE LEARNING APP THAT BUILDS ML MODELS')
+st.info("THIS IS A MACHINE LEARNING APP THAT BUILDS ML MODELS")
 
 
 # --------------------------------------------------
@@ -21,29 +21,25 @@ st.info('THIS IS A MACHINE LEARNING APP THAT BUILDS ML MODELS')
 # --------------------------------------------------
 
 df = pd.read_csv(
-    'https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv'
+    "https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv"
 )
 
 
 # --------------------------------------------------
-# DATA SECTION
+# DATA
 # --------------------------------------------------
 
-with st.expander('Data'):
+with st.expander("Data"):
 
-    st.write('**Raw Data**')
+    st.write("### Raw Data")
     st.dataframe(df)
 
-    st.write('**X**')
-
-    x = df.drop('species', axis=1)
-
+    st.write("### X")
+    x = df.drop("species", axis=1)
     st.dataframe(x)
 
-    st.write('**Y**')
-
-    y = df['species']
-
+    st.write("### Y")
+    y = df["species"]
     st.dataframe(y)
 
 
@@ -51,57 +47,57 @@ with st.expander('Data'):
 # DATA VISUALISATION
 # --------------------------------------------------
 
-with st.expander('Data Visualisation'):
+with st.expander("Data Visualisation"):
 
     st.scatter_chart(
         data=df,
-        x='bill_length_mm',
-        y='body_mass_g',
-        color='species'
+        x="bill_length_mm",
+        y="body_mass_g",
+        color="species"
     )
 
 
 # --------------------------------------------------
-# SIDEBAR - INPUT FEATURES
+# SIDEBAR
 # --------------------------------------------------
 
 with st.sidebar:
 
-    st.header('Input Features')
+    st.header("Input Features")
 
     island = st.selectbox(
-        'Island',
-        ('Torgersen', 'Biscoe', 'Dream')
+        "Island",
+        ["Torgersen", "Biscoe", "Dream"]
     )
 
-    gender = st.selectbox(
-        'Gender',
-        ('male', 'female')
+    sex = st.selectbox(
+        "Sex",
+        ["male", "female"]
     )
 
     bill_length_mm = st.slider(
-        'Bill Length (mm)',
+        "Bill Length (mm)",
         32.1,
         59.6,
         43.9
     )
 
     bill_depth_mm = st.slider(
-        'Bill Depth (mm)',
+        "Bill Depth (mm)",
         13.1,
         21.5,
         17.2
     )
 
     flipper_length_mm = st.slider(
-        'Flipper Length (mm)',
+        "Flipper Length (mm)",
         172.0,
         231.0,
         201.0
     )
 
     body_mass_g = st.slider(
-        'Body Mass (g)',
+        "Body Mass (g)",
         2700.0,
         6300.0,
         4207.0
@@ -109,69 +105,80 @@ with st.sidebar:
 
 
 # --------------------------------------------------
-# CREATE INPUT DATAFRAME
+# INPUT DATAFRAME
 # --------------------------------------------------
 
 data = {
-    'island': island,
-    'bill_length_mm': bill_length_mm,
-    'bill_depth_mm': bill_depth_mm,
-    'flipper_length_mm': flipper_length_mm,
-    'body_mass_g': body_mass_g,
-    'gender': gender
+    "island": [island],
+    "bill_length_mm": [bill_length_mm],
+    "bill_depth_mm": [bill_depth_mm],
+    "flipper_length_mm": [flipper_length_mm],
+    "body_mass_g": [body_mass_g],
+    "sex": [sex]
 }
 
-input_df = pd.DataFrame(data, index=[0])
+input_df = pd.DataFrame(data)
 
 
 # --------------------------------------------------
-# DISPLAY INPUT FEATURES
+# DISPLAY INPUT
 # --------------------------------------------------
 
-with st.expander('Input Features'):
+with st.expander("Input Features"):
 
-    st.write('**Input Penguin**')
+    st.write("### Input Penguin")
 
     st.dataframe(input_df)
 
 
 # --------------------------------------------------
-# MACHINE LEARNING MODEL
+# FEATURES
 # --------------------------------------------------
 
 categorical_features = [
-    'island',
-    'gender'
+    "island",
+    "sex"
 ]
 
 numerical_features = [
-    'bill_length_mm',
-    'bill_depth_mm',
-    'flipper_length_mm',
-    'body_mass_g'
+    "bill_length_mm",
+    "bill_depth_mm",
+    "flipper_length_mm",
+    "body_mass_g"
 ]
 
+
+# --------------------------------------------------
+# PREPROCESSING
+# --------------------------------------------------
 
 preprocessor = ColumnTransformer(
     transformers=[
         (
-            'cat',
-            OneHotEncoder(handle_unknown='ignore'),
+            "categorical",
+            OneHotEncoder(handle_unknown="ignore"),
             categorical_features
         ),
         (
-            'num',
-            'passthrough',
+            "numerical",
+            "passthrough",
             numerical_features
         )
     ]
 )
 
 
+# --------------------------------------------------
+# MODEL
+# --------------------------------------------------
+
 model = Pipeline(
     steps=[
-        ('preprocessor', preprocessor),
-        ('classifier', LogisticRegression(max_iter=1000))
+        ("preprocessor", preprocessor),
+        (
+            "classifier",
+            LogisticRegression(max_iter=1000)
+        )
     ]
 )
 
@@ -189,29 +196,28 @@ model.fit(x, y)
 
 prediction = model.predict(input_df)
 
-
-prediction_probability = model.predict_proba(input_df)
+probability = model.predict_proba(input_df)
 
 
 # --------------------------------------------------
-# DISPLAY PREDICTION
+# RESULT
 # --------------------------------------------------
 
-st.subheader('Prediction')
+st.subheader("Prediction")
 
 st.success(
-    f'The predicted penguin species is: **{prediction[0]}**'
+    f"Predicted Penguin Species: {prediction[0]}"
 )
 
 
 # --------------------------------------------------
-# PREDICTION PROBABILITY
+# PROBABILITY
 # --------------------------------------------------
 
-st.subheader('Prediction Probability')
+st.subheader("Prediction Probability")
 
 probability_df = pd.DataFrame(
-    prediction_probability,
+    probability,
     columns=model.classes_
 )
 
@@ -219,9 +225,7 @@ st.dataframe(probability_df)
 
 
 # --------------------------------------------------
-# BAR CHART
+# PROBABILITY CHART
 # --------------------------------------------------
 
-st.bar_chart(
-    probability_df.T
-)
+st.bar_chart(probability_df.T)
