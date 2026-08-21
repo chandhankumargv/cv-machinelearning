349e09513293b43bf2e2469f3eef6cbafb0667ac
CHANDHAN KUMAR <chandhankumar356@gmail.com>
2026-08-21 14:38:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 st.title('MACHINE LEARNIG APP')
 
 st.info('THIS IS AN MACHINE LERANING APP BUILDS ML MODELS')
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
 
