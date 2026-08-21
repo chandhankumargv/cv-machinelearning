6d9527819c36a7d8c474417149a912daac91ddcf
CHANDHAN KUMAR <chandhankumar356@gmail.com>
2026-08-21 15:21:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,23 @@ with st.expander('Data visualisation'):
 
 with st.sidebar:
   st.header('Input features')
-  # "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
   island = st.selectbox('Island',{'Torgersen','Biscoe','Dream'})
   gender = st.selectbox('Gender',{'male','female'})
   bill_length_mm = st.slider('Bill length {mm}',32.1,59.6,43.9)
+  bill_depth_mm = st.slider('Bill Depth {mm}',13.1,21.5,17.2)
+  flipper_length_mm = st.slider('Flipper lenghth {mm}',172.0,231.0,201.0)
+  body_mass_g = st.slider('Body mass {g}',2700.0,6300,4207.0)
+
+  data = {'island':island,
+          'bill_length_mm':bill_length_mm,
+          'bill_depth_mm':bill_depth_mm,
+          'flipper_length_mm':flipper_length_mm,
+          'body_mass_g':body_mass_g,
+          'gender':gender}
+  input_df = pd.DataFrame(dataindex = [0])
+  input_penguins  = pd.concat([input_df,x],axis = 0))
+with  st.expander('Input feautures'):
+  st.write('**Input penguins**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
