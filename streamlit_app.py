ff0cc8efa64fd0cf7847ae1e7e31386333fd6a36
CHANDHAN KUMAR <chandhankumar356@gmail.com>
2026-08-21 14:50:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,20 @@ import pandas as pd
 st.title('MACHINE LEARNIG APP')
 
 st.info('THIS IS AN MACHINE LERANING APP BUILDS ML MODELS')
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
 
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
+
+  st.write('**X**')
+  x = df.drop('species',axis=1)
+  x
+
+  st.write('**Y**)
+  y = df.species
+  y
+
+with st.expander('Data visualisation'):
+
+  st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
